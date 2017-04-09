e5f32c266de6912cfcfe5859b9dbfd2fe28c6671
Hao Yuan <haoyuan@caen-vnc-vm14.engin.umich.edu>
2017-04-09 16:17:10 -0400

    delet redundant files

--- main.py
+++ main.py
@@ -1,60 +1,14 @@
+import graph
+import LP_relex as LP
+import region_growing as rg
+import naive as na
+import random; random.seed(1)
 
-import tools
-
-# Collect data
-n = 20 # n = 300 works fine
-p = 0.3
-graph = tools.Graph(n = n,p = p)
-edges_weights = tools.get_weights(graph)
-vertices = graph.vertices()
-
-#print(len(edges_weights)) # size of our problem
-
-
-
-from gurobipy import *
-
-edges, weights = multidict(edges_weights)
-
-
-# Create a new model
-m = Model("mip")
-
-# Create variables
-cuts = {} 
-for e in edges:
-    cuts[e] = m.addVar(vtype = GRB.BINARY) # if we cut edge e
-members = {}
-for v in vertices:
-    members[v] = m.addVar(vtype = GRB.BINARY) # if we choose vertex v in our set S
-    
-# Integrate new variables
-m.update()
-
-# Set objective
-m.setObjective(sum(cuts[e]*weights[e] for e in edges), GRB.MINIMIZE) # minimize cut-cost
-
-# Add constraints:
-m.addConstr(members[1] == 1)
-m.addConstr(members[0] == 0)
-for e in edges:
-    u,v = e
-    m.addConstr(members[v] <= members[u] + cuts[e])
-
-# optimize it
-m.optimize()
-
-# display results
-def printSolution():
-    if m.status == GRB.Status.OPTIMAL:
-        print('\nCost: %g' % m.objVal)
-        #buyx = m.getAttr('x', buy)
-        #nutritionx = m.getAttr('x', nutrition)
-        print('\nCut:')
-        for e in edges:
-            print(e, cuts[e].x)
-    else:
-        print('No solution')
-
-printSolution()
-
+G = graph.Graph(n = 15, p = 0.3,st = [(0,1),(2,3)])
+#G.remove_edges([(0,14),(5,12)])
+#dc = {0: [1,2], []}
+#dc = {0: [1,3],1:[0,2,4],2:[1,5],3:[0,4],4:[1,3,5],5:[2,4] }
+#G = graph.Graph(graph_dict = dc,st = [(0,1),(2,4)])
+#print G.is_connected(0,5)
+#print rg.region_growing(G)
+print na.multi_cut_native(G)
