1e7d609a5283e3c6f4c11b1ca2abb75c3c13845a
Parker Koch <pakoch@umich.edu>
2017-04-10 21:27:19 -0400

    Added region_growing_2. Not yet debugged but should work

---
+++ region_growing_2.py
@@ -0,0 +1,35 @@
+import graph
+import math
+
+# G is original graph with weights c, H is the LP solution graph with weights x*
+def region_growing(G,H):
+    c = G.weights()
+    x = H.weights()
+    
+    L = sum([c[e]*x[e] for e in G.edges()])
+    beta = 1.0/len(st_pairs)
+    
+    def volume(d_s,r):
+        B = [v for v in G.vertices() if d_s[v] <= r]
+        Bs = frozenset(B)
+        dB = [e for e in H.edges() if len(e & Bs) == 1]
+        inner = (lambda e : set(e & Bs).pop())  # returns endpt of e belonging to B
+        V = beta*L + sum([c[e]*x[e] for e in H.edges() if len(e & Bs) == 2]) \
+                   + sum([c[e]*(r - d_s[inner(e)]) for e in dB])
+        return (V,dB)
+    
+    def cost(S):
+        return sum([c[e] for e in S])
+    
+    F = []
+    for s,t in G.sts():
+        d_s = H.dist_from(s)
+        if d_s[t] < float("inf"):
+            for r in sorted(d_s.values()):
+                V,dB = volume(d_s,r)
+                if cost(dB) <= V*2*math.log((beta+1)/beta):
+                    F = F + dB
+                    H.remove_edges_from(dB)
+                    break
+    return F
+
